0b2190df264c01d3b3cc4d312e0d284e0560cd5d
Mike Peiman <mikepeiman@gmail.com>
2019-03-07 17:10:34 -0800

    Got the 'biglink' effect working reliably, finally - due to using :focus-within, my mouseover and click functions were giving the dropdown parent element as final value

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,49 @@
 <template>
-  <header class="header">
-    <g-link id="site-logo" class="site-title" :to="{ name: 'home' }">
-      <g-image  alt="logo" src="../assets/LifeIQ.svg" width="60" />
-    </g-link>
-    
-    <nav role="navigation">
-      <ul class="nav-items">
-        <li @click="bigLink" @mouseover="mouseOver(2)" class="nav-link-container"><g-link class="nav__link" :to="{ name: 'home' }">Home</g-link></li>
-        <li @click="bigLink" @mouseover="mouseOver(3)"  class="nav-link-container"><g-link class="nav__link" :to="{ name: 'about' }">About</g-link></li>
-        <li @click="dataSourcesLink" class="nav-link-container"><g-link class="nav__link" :to="{ name: 'dataSources' }">Data Sources</g-link>
-          <ul class="dropdown" aria-label="submenu">
-            <li @click="bigLink" @mouseover="mouseOver(5)"  class="nav-link-container"><g-link class="nav__link" :to="{ name: 'fakerPosts' }">Faker</g-link></li>
-            <li @click="bigLink" @mouseover="mouseOver(6)"  class="nav-link-container"><g-link class="nav__link" :to="{ name: 'placeholder' }">Placeholder</g-link></li>
-            <li @click="bigLink" @mouseover="mouseOver(7)"  class="nav-link-container"><g-link class="nav__link" :to="{ name: 'starwars' }">StarWars</g-link></li>
-            <li @click="bigLink" @mouseover="mouseOver(8)"  class="nav-link-container"><g-link class="nav__link" :to="{ name: 'dota' }">DOTA2</g-link></li>
-            <li @click="bigLink" @mouseover="mouseOver(9)"  class="nav-link-container"><g-link class="nav__link" :to="{ name: 'filesystem' }">Filesystem</g-link></li>
-            <li @click="bigLink" @mouseover="mouseOver(10)"  class="nav-link-container"><g-link class="nav__link" :to="{ name: 'airDb' }">Airtable</g-link></li>
-            <li @click="bigLink" @mouseover="mouseOver(11)"  class="nav-link-container"><g-link class="nav__link" :to="{ name: 'graph' }">GraphCMS</g-link></li>
-          </ul>
-        </li>
-        <li @click="bigLink" @mouseover="mouseOver(12)"  class="nav-link-container"><g-link class="nav__link" :to="{ name: 'contact' }">Contact</g-link></li>
-      </ul>
-    </nav>
-  </header>
+<header class="header">
+  <g-link id="site-logo" class="site-title" :to="{ name: 'home' }">
+    <g-image alt="logo" src="../assets/LifeIQ.svg" width="60" />
+  </g-link>
+
+  <nav role="navigation">
+    <ul class="nav-items">
+      <li @click="mainLink(2)" @mouseover="mouseOver(2)" class="nav-link-container">
+        <g-link class="nav__link" :to="{ name: 'home' }">Home</g-link>
+      </li>
+      <li @click="mainLink(3)" @mouseover="mouseOver(3)" class="nav-link-container">
+        <g-link class="nav__link" :to="{ name: 'about' }">About</g-link>
+      </li>
+      <li @click="mainLink(4)" @mouseover="mouseOver(4)" class="nav-link-container">
+        <g-link class="nav__link" :to="{ name: 'dataSources' }">Data Sources</g-link>
+        <ul class="dropdown" aria-label="submenu">
+          <li @click="subLink(5)" @mouseover="mouseOver(5)" class="nav-link-container">
+            <g-link class="nav__link" :to="{ name: 'fakerPosts' }">Faker</g-link>
+          </li>
+          <li @click="subLink(6)" @mouseover="mouseOver(6)" class="nav-link-container">
+            <g-link class="nav__link" :to="{ name: 'placeholder' }">Placeholder</g-link>
+          </li>
+          <li @click="subLink(7)" @mouseover="mouseOver(7)" class="nav-link-container">
+            <g-link class="nav__link" :to="{ name: 'starwars' }">StarWars</g-link>
+          </li>
+          <li @click="subLink(8)" @mouseover="mouseOver(8)" class="nav-link-container">
+            <g-link class="nav__link" :to="{ name: 'dota' }">DOTA2</g-link>
+          </li>
+          <li @click="subLink(9)" @mouseover="mouseOver(9)" class="nav-link-container">
+            <g-link class="nav__link" :to="{ name: 'filesystem' }">Filesystem</g-link>
+          </li>
+          <li @click="subLink(10)" @mouseover="mouseOver(10)" class="nav-link-container">
+            <g-link class="nav__link" :to="{ name: 'airDb' }">Airtable</g-link>
+          </li>
+          <li @click="subLink(11)" @mouseover="mouseOver(11)" class="nav-link-container">
+            <g-link class="nav__link" :to="{ name: 'graph' }">GraphCMS</g-link>
+          </li>
+        </ul>
+      </li>
+      <li @click="mainLink(12)" @mouseover="mouseOver(12)" class="nav-link-container">
+        <g-link class="nav__link" :to="{ name: 'contact' }">Contact</g-link>
+      </li>
+    </ul>
+  </nav>
+</header>
 </template>
 
 <static-query>
@@ -39,7 +60,9 @@ export default {
   data: function () {
     return {
       activeLink: '',
-      dropdown: false,
+      lastActiveLink: '',
+      submenuActive: false,
+      linkSet: []
     }
   },
   mounted() {
@@ -49,16 +72,52 @@ export default {
     dropdownTest() {
       console.log('dropdown test')
     },
-    bigLink() {
-      console.log(`Current activelink: ${this.activeLink}`);
-      this.$router.push(this.activeLink)
+    mainLink(x) {
+      let el = Object.values(this.$refs)[x].$el;
+      let elClass = el.parentElement.parentElement.attributes.class.value
+      let link = el.attributes.href.value
+      this.linkSet.push(link)
+      let i = this.linkSet.length;
+      this.activeLink = link;
+      console.log(`MAINLINK - Current activelink: ${this.activeLink}, current click: ${link}, linkSet: ${this.linkSet}`);
+      if (this.linkSet.length !== 1) {
+        this.$router.push(this.linkSet[0]);
+      } else {
+        this.$router.push(this.activeLink);
+      }
+
+      this.linkSet = []
     },
-        dataSourcesLink() {
+    subLink(x) {
+
+      let el = Object.values(this.$refs)[x].$el;
+      let elClass = el.parentElement.parentElement.attributes.class.value
+      let link = el.attributes.href.value
+      this.linkSet.push(link)
+      let i = this.linkSet.length;
+      this.activeLink = link;
+      console.log(`subLINK - Current activelink: ${this.activeLink}, current click: ${link}, linkSet: ${this.linkSet}`);
+      this.$router.push(this.activeLink);
+
+    },
+    dataSourcesLink() {
+      console.log(`Current activelink: ${this.activeLink}`);
       this.$router.push("/data-sources");
     },
     mouseOver(x) {
-      console.log(Object.values(this.$refs)[x].$el.attributes.href.value);
-      this.activeLink = Object.values(this.$refs)[x].$el.attributes.href.value;
+      // let el = Object.values(this.$refs)[x].$el;
+      // let elClass = el.parentElement.parentElement.attributes.class.value
+      // let link = el.attributes.href.value
+      // this.linkSet.push(link)
+      // let i = this.linkSet.length;
+      // // console.log('Current mouseOver link: ', link)
+      // // console.log('Current mouseOver link $el grandparent: ', elClass)
+      // this.lastActiveLink = this.activeLink;
+      // this.activeLink = link;
+      // if(elClass.includes("drop")) {
+      //   // console.log(`This list item,** ${this.linkSet[i-1]} ** is a child of dropdown`)
+      //   this.activeLink = this.linkSet[i-1];
+      // }
     }
   },
   computed: {
@@ -67,7 +126,7 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 $primary-graphite: #252525; // rgba($primary-graphite,1)
 $primary-blue: #00A1FF; // rgba($primary-blue,1)
 $primary-orange:#FF7800; // rgba($primary-orange,1)
@@ -84,6 +143,7 @@ $primary-purple: #4600CD; // rgba($primary-purple,1)
   width: 100%;
   // height: 70px;
 }
+
 a {
   text-decoration: none;
 }
@@ -97,7 +157,7 @@ nav {
   font-family: monospace;
   font-size: .9rem;
   // border-bottom: 3px solid $primary-blue;
-  width: 760px; 
+  width: 760px;
 }
 
 ul {
@@ -105,6 +165,7 @@ ul {
   list-style: none;
   margin: 0;
 }
+
 .nav-items {
   display: flex;
   padding-left: 0.5rem;
@@ -126,7 +187,7 @@ li.nav-link-container {
   transition-duration: 0.25s;
   list-style: none;
   margin-bottom: 0;
-  border-bottom: 3px solid rgba(0,0,0,0);
+  border-bottom: 3px solid rgba(0, 0, 0, 0);
   width: 16ch;
   height: 3ch;
   text-align: center;
@@ -139,7 +200,7 @@ li.nav-link-container a {
 
 li.nav-link-container:hover,
 li.nav-link-container:focus-within {
-  background: rgba($primary-green,0.25);
+  background: rgba($primary-green, 0.25);
   cursor: pointer;
   border-bottom: 1px solid $primary-green;
   /* width: 17ch; */
@@ -180,6 +241,7 @@ ul li ul:focus {
   padding: 8% 8% 8% 8%;
   transition: all .25s ease-in-out;
 }
+
 .dropdown li:hover {
   clear: both;
   width: 80%;
@@ -188,9 +250,10 @@ ul li ul:focus {
   transition: all .25s ease-in-out;
   // transform: translateX(2em);
   padding: 8% 8% 8% 12%;
-  a{
+
+  a {
     // padding: 8% 8% 8% 12%;
-    
+
     transition: all .25s ease-in-out;
   }
 }
