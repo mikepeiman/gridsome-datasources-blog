0089da0aca5bdf5887221cf19ecee5fe013bfcc2
Mike Peiman <mikepeiman@gmail.com>
2019-03-09 15:53:17 -0800

    Updated Airtable single view

---
+++ src/templates/Airtable.1.vue
@@ -0,0 +1,26 @@
+<template>
+  <Layout>
+    <h2>{{$page.post.resort}}</h2>
+    <h3>{{$page.post.price}}: {{$page.post.currency}}</h3>
+  </Layout>
+</template>
+
+<page-query>
+query Airtable ($path: String!){
+  post: airtable (path: $path){
+    resort
+    price
+    currency
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.post.title
+    }
+  }
+}
+</script>
