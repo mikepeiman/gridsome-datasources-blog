1f9c4df09402f0901277667e244e7894558745b0
Mike Peiman <mikepeiman@gmail.com>
2019-04-05 11:05:53 -0700

    Refactored login to accept arguments and added getPosts function successfully

--- src/sources/strapi/index.js
+++ src/sources/strapi/index.js
@@ -16,20 +16,20 @@ const axios = require("axios");
 //   // access the value inside the `then`
 // })
 
-
-function login() {
+function login(user, password) {
   let token = ''
   return axios
   .post('http://localhost:1337/auth/local', {
-      identifier: 'mikepeiman@gmail.com',
-      password: '326463'
+      identifier: user,
+      password: password
   })
   .then(response => {
     // Handle success.
     console.log('Well done!');
     console.log('User profile', response.data.user);
     console.log('User token', response.data.jwt);
-    return response.data.jwt
+    token = response.data.jwt
+    return token
   })
   .catch(error => {
     // Handle error.
@@ -37,23 +37,72 @@ function login() {
   });
 }
 
-login().then(function(token) {
-  console.log(`New function token: ${token}`)
+function getPosts(user, password) {
+  login(user, password).then(token => {
+  console.log(`token from calling login(): ${token}`)
+  axios
+  .get('http://localhost:1337/posts', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  .then(response => {
+    // Handle success.
+    console.log('Data: ', response.data);
+  })
+  .catch(error => {
+    // Handle error.
+    console.log('An error occurred:', error);
+  });
 })
-let token2 = login()
+}
+
+getPosts('mikepeiman@gmail.com', '326463')
+
+
+// async function getToken() {
+//   try {
+//     await login()
+//   } catch(err) {
+//     console.log(`getToken error: ${err}`)
+//     return 'Error in getToken()'
+//   }
+// }
+
+// let token = login().then(res => {
+//   return res
+// })
+// let token = getToken()
+
+// console.log(`getToken: ${token}`)
+
+// login().then(token => {
+//   console.log(`New function token: ${token}`)
+// })
+
+// let token2 = login()
 
 // const token2 = login().then(token => {
 //   console.log(`token from calling login(): ${token}`)
 // })
+// console.log(`token2 = ${token2}`)
 
-const token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTU0NDA2MDk5LCJleHAiOjE1NTY5OTgwOTl9.HVOhxQ1S0SK2PxCtJa80nEi3eHJgLWcB73ekFhKUSd8';
+// const token2 = Promise.resolve(login())
 
-function myHandler(token) {
-  console.log(`myHandler of token result: ${token}`)
-}
-myHandler(login())
+// token2.then(token => {
+//   console.log(`token from calling login(): ${token}`)
+// })
+// console.log(`token2 = ${token2}`)
 
-console.log(`token1 from login.js: ${token1}, token2 from response.data.jwt: ${token2}`)
+const givenToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTU0NDA2MDk5LCJleHAiOjE1NTY5OTgwOTl9.HVOhxQ1S0SK2PxCtJa80nEi3eHJgLWcB73ekFhKUSd8';
+
+// function getToken(token) {
+//   console.log(`getToken of token result: ${token}`)
+//   return Promise.token
+// }
+// getToken(login())
+// let token = await getToken(login()).then(token => result.data)
+// console.log(`Trying to set token to a global variable: ${token}`)
 
 
 // module.exports = function(api) {
