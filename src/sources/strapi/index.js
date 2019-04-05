d353906cc106c6a3624275507d81fcd57b7290a7
Mike Peiman <mikepeiman@gmail.com>
2019-04-05 11:45:08 -0700

    Completed Strapi data source, confirmed in GraphQL playground

--- src/sources/strapi/index.js
+++ src/sources/strapi/index.js
@@ -1,162 +1,113 @@
 const axios = require("axios");
-// const login = require("./login");
-// const { GraphQLSchema, buildSchema, GraphQLObjectType } = require("graphql");
-// Use the Data store API here: https://gridsome.org/docs/data-store-api
-
-// Request API.
-
-// function foo() {
-//   // RETURN the promise
-//   return fetch("/echo/json").then(function(response){
-//       return response.json(); // process it inside the `then`
-//   });
-// }
-
-// foo().then(function(response){
-//   // access the value inside the `then`
-// })
+const {
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLString,
+  GraphQLInt,
+  buildSchema,
+  printSchema,
+  GraphQLObjectType
+} = require("graphql");
 
 function login(user, password) {
-  let token = ''
+  let token = "";
   return axios
-  .post('http://localhost:1337/auth/local', {
+    .post("http://localhost:1337/auth/local", {
       identifier: user,
       password: password
-  })
-  .then(response => {
-    // Handle success.
-    console.log('Well done!');
-    console.log('User profile', response.data.user);
-    console.log('User token', response.data.jwt);
-    token = response.data.jwt
-    return token
-  })
-  .catch(error => {
-    // Handle error.
-    console.log('An error occurred:', error);
-  });
+    })
+    .then(response => {
+      // Handle success.
+      // console.log('Well done!');
+      // console.log('User profile', response.data.user);
+      // console.log('User token', response.data.jwt);
+      token = response.data.jwt;
+      return token;
+    })
+    .catch(error => {
+      // Handle error.
+      console.log("An error occurred:", error);
+    });
 }
 
 function getPosts(user, password) {
-  login(user, password).then(token => {
-  console.log(`token from calling login(): ${token}`)
-  axios
-  .get('http://localhost:1337/posts', {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  })
-  .then(response => {
-    // Handle success.
-    console.log('Data: ', response.data);
-  })
-  .catch(error => {
-    // Handle error.
-    console.log('An error occurred:', error);
+  return login(user, password).then(token => {
+    return axios
+      .get("http://localhost:1337/posts", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(response => {
+        // Handle success.
+        return response.data;
+      })
+      .catch(error => {
+        // Handle error.
+        console.log("An error occurred:", error);
+      });
   });
-})
 }
 
-getPosts('mikepeiman@gmail.com', '326463')
+module.exports = function(api) {
+  console.log("Datasource loading: Strapi");
 
+  api.loadSource(async store => {
+    const contentType = store.addContentType({
+      typeName: "Strapi",
+      route: "/strapi/:id"
+    });
+    const Category = new GraphQLObjectType({
+      name: 'Category',
+      description: 'A way to categorize content',
+      fields: () => ({
+        name: {
+          type: GraphQLString,
+          resolve: category => category.name
+        },
+      })
+    })
 
-// async function getToken() {
-//   try {
-//     await login()
-//   } catch(err) {
-//     console.log(`getToken error: ${err}`)
-//     return 'Error in getToken()'
-//   }
-// }
+    contentType.addSchemaField("title", ({ graphql }) => ({
+      type: graphql.GraphQLString,
+      allowNull: false,
+      resolve(node) {
+        return node.fields.title;
+      }
+    }));
+    contentType.addSchemaField("subtitle", ({ graphql }) => ({
+      type: graphql.GraphQLString,
+      resolve(node) {
+        return node.fields.subtitle;
+      }
+    }));
+    contentType.addSchemaField("body", ({ graphql }) => ({
+      type: graphql.GraphQLString,
+      resolve(node) {
+        return node.fields.body;
+      }
+    }));
+    contentType.addSchemaField("categories", ({ graphql }) => ({
+      type: graphql.GraphQLList(Category),
+      resolve(node) {
+        return node.fields.categories;
+      }
+    }));
 
-// let token = login().then(res => {
-//   return res
-// })
-// let token = getToken()
-
-// console.log(`getToken: ${token}`)
-
-// login().then(token => {
-//   console.log(`New function token: ${token}`)
-// })
-
-// let token2 = login()
-
-// const token2 = login().then(token => {
-//   console.log(`token from calling login(): ${token}`)
-// })
-// console.log(`token2 = ${token2}`)
-
-// const token2 = Promise.resolve(login())
-
-// token2.then(token => {
-//   console.log(`token from calling login(): ${token}`)
-// })
-// console.log(`token2 = ${token2}`)
-
-const givenToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTU0NDA2MDk5LCJleHAiOjE1NTY5OTgwOTl9.HVOhxQ1S0SK2PxCtJa80nEi3eHJgLWcB73ekFhKUSd8';
-
-// function getToken(token) {
-//   console.log(`getToken of token result: ${token}`)
-//   return Promise.token
-// }
-// getToken(login())
-// let token = await getToken(login()).then(token => result.data)
-// console.log(`Trying to set token to a global variable: ${token}`)
-
-
-// module.exports = function(api) {
-//   console.log('Datasource loading: Strapi')
-  
-//   api.loadSource(async store => {
-//     const contentType = store.addContentType({
-//       typeName: 'Strapi',
-//       route: '/strapi/:id'
-//     });
-
-//     contentType.addSchemaField("name", ({ graphql }) => ({
-//       type: graphql.GraphQLString,
-//       allowNull: false,
-//       resolve(node) {
-//         return node.fields.name;
-//       }
-//     }));
-//     contentType.addSchemaField("starship_class", ({ graphql }) => ({
-//       type: graphql.GraphQLString,
-//       resolve(node) {
-//         return node.fields.starshipClass;
-//       }
-//     }));
-//     contentType.addSchemaField("url", ({ graphql }) => ({
-//       type: graphql.GraphQLString,
-//       resolve(node) {
-//         return node.fields.url;
-//       }
-//     }));
-//     contentType.addSchemaField("path", ({ graphql }) => ({
-//       type: graphql.GraphQLString,
-//       resolve(node) {
-//         return node.fields.path;
-//       }
-//     }));
-
-//     const { data } = await axios.get("http://localhost:1337/posts");
-//     data.results.forEach(item => {
-//       console.log(`Item from STRAPI posts: ${item}`)
-      
-//       let pathArray = item.url.split("/");
-//       id = pathArray[5];
-//       let path = `/strapi/${id}`
-//       contentType.addNode({
-//         date: item.date,
-//         path,
-//         fields: {
-//           id: id,
-//           name: item.name,
-//           starship_class: item.starship_class,
-//           url: item.url,
-//         }
-//       });
-//     });
-//   });
-// };
+    getPosts("mikepeiman@gmail.com", "326463").then(data => {
+      console.log(`final .then data: ${data}`);
+      data.forEach(item => {
+        contentType.addNode({
+          date: item.created_at,
+          fields: {
+            id: item.id,
+            title: item.title,
+            subtitle: item.subtitle,
+            body: item.body,
+            categories: item.categories
+          }
+        });
+      });
+    });
+  });
+};
